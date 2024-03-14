c3152c681ae91e1f87eada258eb3174f91715bda
Zeno Gallegos <zenogallegos@Zeno-MacBook-Pro.local>
2024-03-14 09:34:49 -0600

    finished generator and prepped for video recording

---
+++ dist/README.md
@@ -0,0 +1,29 @@
+# a
+  ![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+  
+* [License](#license) 
+
+  ## License
+      Licensed with MIT
+## Table of Contents
+  * [Description](#description)
+  * [Media](#media)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributors](#contributors)
+  * [Tests](#tests)
+  * [Questions](#questions)
+## Description
+a
+## Media
+a
+## Installation
+a
+## Usage
+a
+## Contributors
+a
+## Testing
+a
+## Questions
+a
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,14 +4,14 @@ function renderLicenseBadge(license) {
   if (license !== "None") {
     return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   }
-  return "";
+  return ``;
 }
 
 // Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license != "None") {
-  return `In* [License] (#license) \n`;
+  return `\n* [License](#license) \n`;
   }
   return "";
 }
@@ -19,9 +19,9 @@ function renderLicenseLink(license) {
 // Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license != "none") {
+  if (license != "None") {
     return `## License
-    Licensed with ${license}`;
+      Licensed with ${license}`;
   }
   return "";
 }
